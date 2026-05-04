c0e84a79c2ffbe23892fdb61e88032b21047766b
MarioJGC <77269474mario@gmail.com>
2026-05-04 12:13:09 +0200

    Eliminar limite de tamanio en subida de ficheros para cifrado

--- src/main/java/controlador/ServletCifradoContenido.java
+++ src/main/java/controlador/ServletCifradoContenido.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @WebServlet(name = "ServletCifradoContenido", urlPatterns = {"/ServletCifradoContenido"})
-@MultipartConfig(location = "/tmp", maxFileSize = 104857600)
+@MultipartConfig(location = "/tmp", maxFileSize = -1, maxRequestSize = -1)
 public class ServletCifradoContenido extends HttpServlet {
 
     @Override
